c0b9917bcd49709ea6a658a284620f86c0913273
Ilya Stritskiy <msv_ilya@mail.ru>
2024-07-17 14:01:04 +0300

    task 1 was solved

--- task_1.js
+++ task_1.js
@@ -1,1 +1,20 @@
-// this file will be filled
+function getPropertiesOfObject(obj){
+    for (let key in obj){
+        if (obj.hasOwnProperty(key)){
+            console.log(`Property - ${key}; Value - ${obj[key]} \n`)
+        }
+    }
+}
+
+
+let prototype = {
+    university: 'MGIMO',
+};
+
+let student = Object.create(prototype);
+student.name = 'Ilya';
+student.age = 21;
+student.faculty = 'journalism';
+
+
+getPropertiesOfObject(student)
