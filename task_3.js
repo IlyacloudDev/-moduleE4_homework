9b89976fbee0eda6be7d5df8daaf84c8136f2922
Ilya Stritskiy <msv_ilya@mail.ru>
2024-07-20 15:30:36 +0300

    some fixed mistakes with solving the task

--- task_3.js
+++ task_3.js
@@ -1,11 +1,9 @@
 let emptyObject = () => {
-    let result = new Object;
-    return result
+    let result = Object.create(null);
+    return result;
 }
 
-
 let newObject = emptyObject();
-
 
 console.log(newObject);
 console.log(Object.getPrototypeOf(newObject));
